451727ac843c2afce8be89877e812e8bc542726a
dan548 <danfromomsk@gmail.com>
2018-06-12 10:07:10 +0600

    LinkedHashTable done!

--- Sem4C/LinkedHashTable.h
+++ Sem4C/LinkedHashTable.h
@@ -1,72 +1,213 @@
 #pragma once
-const int SIZE = 17;
+#include "stdafx.h"
+
+struct ValueNotFoundException {
+	std::string message;
+	ValueNotFoundException() {
+		message = "Value not found.";
+	}
+
+	ValueNotFoundException(const char* cMessage) {
+		message = cMessage;
+	}
+};
+
+struct EmptyHashTableException {
+	std::string message;
+	EmptyHashTableException(const char* cMessage) {
+		message = cMessage;
+	}
+};
+
+const int SIZE = 1 << 4;
 template <typename K, typename V>
 class LinkedHashTable {
-private:
-	template <typename K, typename V>
+public:
 	struct Node {
 		K key;
 		V value;
 		Node* next;
 		Node* nextGlobal;
-		Node(K k, V val) : key(k), value(val), next(nullptr), 
-			nextGlobal(nullptr) {}
+		Node* prevGlobal;
+		Node(K k, V val) : key(k), value(val), next(nullptr),
+			nextGlobal(nullptr), prevGlobal(nullptr) {}
 	};
-	template <typename K, typename V>
+private:
 	class LinkedHashEntry {
 	public:
 		LinkedHashEntry(Node* elem) {
 			beginEntry = elem;
 		}
 		void addEntry(Node* elem) {
-			Node<K, V>* tmp = beginEntry;
+			if (beginEntry == nullptr) beginEntry = elem;
+			Node* tmp = beginEntry;
 			while (tmp->next != nullptr) {
 				tmp = tmp->next;
 			}
-			tmp->next = elem;
+			if (tmp->key == elem->key) {
+				tmp->value = elem->value;				
+			}
+			else {
+				tmp->next = elem;
+			}			
 		}
-		Node<K, V>* beginEntry;
+		Node* beginEntry = nullptr;
 	};
-	int hashCode(const K& elem) {
-		return func(elem) % SIZE;
-	}
 	LinkedHashEntry* *table;
 	size_t length;
-	Node<K,V>* begin;
+	Node* begin;
+	Node* end;
 	int(*func) (const K&);
-public:
-	LinkedHashTable(int(*f) (const K&)) {
-		table = new LinkedHashEntry*[SIZE];
-		current = nullptr;
-		length = 0;
-		func = f;
+	int size;
+	int hashCode(const K& elem) {
+		return func(elem) % size;
 	}
-	void add(const K& key, const V& value) {
+public:
+	LinkedHashTable(int(*f) (const K&), int size = SIZE):
+	size(size), begin(nullptr), end(nullptr), length(0), func(f){
+		table = new LinkedHashEntry*[size];
+		for (int i = 0; i < size; i++) {
+			table[i] = nullptr;
+		}
+	}
+	void push(const K& key, const V& value) {
 		length++;
-		Node<K,V>* tmp = new Node(key, value);
-		end->nextGlobal = tmp;
-		end = tmp;
+		bool repeat = false;
+		Node* tmp = new Node(key, value);
 		int hash = hashCode(key);
 		if (table[hash] == nullptr) {
 			table[hash] = new LinkedHashEntry(tmp);
 		}
 		else {
+			Node* tmp2 = table[hash]->beginEntry;
+			if (tmp2 != nullptr) {
+				if (tmp2->key == key) {
+					length--;
+					repeat = true;
+				}
+				else {
+					while (tmp2->next != nullptr) {
+						if (tmp2->next->key != key) {
+							tmp2 = tmp2->next;
+							continue;
+						}
+						else {
+							length--;
+							repeat = true;
+							break;
+						}
+					}
+				}
+			}
 			table[hash]->addEntry(tmp);
 		}
+		if (!repeat) {
+			if (begin == nullptr) {
+				begin = tmp;
+			}
+			else {
+				if (end == nullptr) {
+					tmp->prevGlobal = begin;
+					end = tmp;
+					begin->nextGlobal = end;
+				}
+				else {
+					tmp->prevGlobal = end;
+					end->nextGlobal = tmp;
+					end = tmp;
+				}
+			}
+		}
 	}
-	void remove(const K& elem) {
+	V pop(const K& elem) {
+		if (isEmpty()) throw EmptyHashTableException("The table is empty!");
 		int hash = hashCode(elem);
 		if (table[hash] != nullptr) {
-			Node<K, V>* tmp = table[hash]->beginEntry;
+			Node* tmp = table[hash]->beginEntry;
+			Node* tmp2 = tmp;
+			if (tmp->key == elem) {
+				table[hash]->beginEntry = tmp->next;
+			}
+			else {
+				while (tmp2->next != nullptr) {
+					if (tmp2->next->key != elem) {
+						tmp2 = tmp2->next;
+						continue;
+					}
+					else {
+						tmp = tmp2->next;
+						tmp2->next = tmp->next;
+					}
+				}
+				throw ValueNotFoundException();
+			}
+			if (tmp->prevGlobal == nullptr) {
+				begin = begin->nextGlobal;
+				begin->prevGlobal = nullptr;
+			}
+			else {
+				if (tmp == end) {
+					end = tmp->prevGlobal;
+				}
+				tmp->prevGlobal->nextGlobal = tmp->nextGlobal;
+			}
+			V valuePop = tmp->value;
+			delete tmp;
+			length--;
+			return valuePop;
+		}
+		else {
+			throw ValueNotFoundException();
 		}
 	}
 	V get(const K& elem) {
-
+		if (isEmpty()) throw EmptyHashTableException("The table is empty!");
+		int hash = hashCode(elem);
+		if (table[hash] != nullptr) {
+			Node* tmp = table[hash]->beginEntry;
+			Node* tmp2 = tmp;
+			if (tmp->key == elem) {
+				V value = tmp->value;
+				return value;
+			}
+			else {
+				while (tmp2->next != nullptr) {
+					if (tmp2->next->key != elem) {
+						tmp2 = tmp2->next;
+						continue;
+					}
+					else {
+						V value = tmp->value;
+						return value;
+					}
+				}
+				throw ValueNotFoundException();
+			}
+		}
+		else {
+			throw ValueNotFoundException();
+		}
 	}
 	void makeEmpty() {
-
+		Node* tmp = end;
+		while (!isEmpty()) {
+			if (tmp->prevGlobal != nullptr) {
+				tmp = tmp->prevGlobal;
+				end = tmp;
+				delete tmp->nextGlobal;
+			}
+			else {
+				delete tmp;
+			}
+			length--;	
+		}
+		begin = nullptr;
+		end = nullptr;
+		for (int i = 0; i < size; i++) {
+			table[i] = nullptr;
+		}
 	}
 	bool isEmpty() {
-
+		return length == 0;
 	}
 };
